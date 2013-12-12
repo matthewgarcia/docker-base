07d039d6634cd691a7bfcfd9e56f6228bb113915
Kirk True <kirk@mustardgrain.com>
2013-12-12 14:55:15 -0800

    Added curl installation and sudo-itization of a couple of commands

--- docker-setup/setup-vagrant.sh
+++ docker-setup/setup-vagrant.sh
@@ -3,14 +3,14 @@
 vagrant up
 
 vagrant ssh -c "sudo apt-get update"
-vagrant ssh -c "sudo apt-get -y install linux-image-generic-lts-raring linux-headers-generic-lts-raring"
+vagrant ssh -c "sudo apt-get -y install curl linux-image-generic-lts-raring linux-headers-generic-lts-raring"
 
 vagrant reload
 
-vagrant ssh -c "curl https://get.docker.io/gpg | apt-key add -"
-vagrant ssh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+vagrant ssh -c "curl https://get.docker.io/gpg | sudo apt-key add -"
+vagrant ssh -c "sudo sh -c 'echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list'"
 vagrant ssh -c "sudo apt-get update"
-vagrant ssh -c "sudo apt-get install lxc-docker"
+vagrant ssh -c "sudo apt-get -y install lxc-docker"
 
 vagrant ssh -c "sudo docker pull ubuntu"
 vagrant ssh -c "sudo docker pull dhrp/sshd"
